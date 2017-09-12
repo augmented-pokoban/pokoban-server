268c51037e544645e3930722e4de37922cc633b6
Troels Thomsen <tt@ttsoftware.net>
2017-09-12 21:24:02 +0200

    first commit

---
+++ src/main/java/server.kt
@@ -0,0 +1,26 @@
+import model.Pokoban
+import javax.ws.rs.*
+import javax.ws.rs.core.MediaType
+
+@Path("/")
+class PokobanService {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    fun index(): List<Pokoban> {
+        return listOf<Pokoban>(Pokoban())
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    fun show(@PathParam("id") id: String): Pokoban {
+        return Pokoban()
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    fun create(): Pokoban {
+        return Pokoban()
+    }
+}
