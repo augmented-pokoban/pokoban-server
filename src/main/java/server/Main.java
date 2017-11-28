0caf76a03edec38e1031450812ea1aea85dbfe98
Troels Thomsen <tt@ttsoftware.net>
2017-11-28 13:37:46 +0100

    Adds angular router forward

--- src/main/java/server/Main.java
+++ src/main/java/server/Main.java
@@ -4,11 +4,19 @@ import org.apache.catalina.authenticator.jaspic.AuthConfigFactoryImpl;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.security.auth.message.config.AuthConfigFactory;
 
+@Controller
 @SpringBootApplication
 public class Main extends SpringBootServletInitializer {
+
+    @RequestMapping(value = "/**/{[path:[^\\.]*}")
+    public String redirectUi() {
+        return "forward:/index.html";
+    }
 
     public static void main(String[] args) throws Exception {
         if (AuthConfigFactory.getFactory() == null) {
